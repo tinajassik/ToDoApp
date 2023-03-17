92e46d924f1688129578d691369b1d723aea23c6
Kristina Jassova <kristinka.jassova@gmail.com>
2023-03-17 18:26:33 +0100

    Blazor project set up + Create User Razor Page

---
+++ BlazorServerApp/Pages/CreateUser.razor.cs
@@ -0,0 +1,32 @@
+﻿using Domain.DTOs;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorServerApp.Pages
+{
+    public partial class CreateUser : ComponentBase
+    {
+        private string username = "";
+        private string resultMsg = "";
+        private string color = "";
+    
+        private async Task Create()
+        {
+            resultMsg = "";
+
+            try
+            {
+                await userService.Create(new UserCreationDTO(username));
+                username = "";
+                resultMsg = "User successfully created";
+                color = "green";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                resultMsg = e.Message;
+                color = "red";
+            }
+        }
+    
+    }
+}
