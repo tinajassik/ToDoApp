b4637f107ad4f45170032119f1d6627fa43cab1c
Kristina Jassova <kristinka.jassova@gmail.com>
2023-03-19 13:29:58 +0100

    View filtered todos page issue: userId filtering

--- HttpClients/ClientInterfaces/ITodoService.cs
+++ HttpClients/ClientInterfaces/ITodoService.cs
@@ -6,4 +6,10 @@ namespace HttpClients.ClientInterfaces;
 public interface ITodoService
 {
     Task<Todo> CreateAsync(TodoCreationDto dto);
+    Task<ICollection<Todo>> GetAsync(
+        string? userName, 
+        int? userId, 
+        bool? completedStatus, 
+        string? titleContains
+    );
 }
--- HttpClients/Implementations/TodoHttpClient.cs
+++ HttpClients/Implementations/TodoHttpClient.cs
@@ -30,4 +30,59 @@ public class TodoHttpClient : ITodoService
                 })!;
         return todo;
     }
+
+    // task ? User Id does snot work :( 
+    public async Task<ICollection<Todo>> GetAsync(string? userName, int? userId, bool? completedStatus, string? titleContains)
+    {
+        string query = ConstructQuery(userName, userId, completedStatus, titleContains);
+        HttpResponseMessage responseMessage = await client.GetAsync("/todos"+query);
+        string content = await responseMessage.Content.ReadAsStringAsync();
+
+        if (!responseMessage.IsSuccessStatusCode)
+        {
+            throw new Exception(content);
+        }
+
+        ICollection<Todo> todos = JsonSerializer.Deserialize<ICollection<Todo>>(content,
+            new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            })!;
+        
+        return todos; 
+    }
+    
+    
+    // checks each filter argument
+    // checks if null --> in which case they should be ignored.
+    // otherwise include the needed filter argument in the query parameter string.
+    private static string ConstructQuery(string? userName, int? userId, bool? completedStatus, string? titleContains)
+    {
+        
+        string query = "";
+        if (!string.IsNullOrEmpty(userName))
+        {
+            query += $"?username={userName}";
+        }
+
+        if (userId != null)
+        {
+            query += string.IsNullOrEmpty(query) ? "?" : "&";
+            query += $"userid={userId}";
+        }
+
+        if (completedStatus != null)
+        {
+            query += string.IsNullOrEmpty(query) ? "?" : "&";
+            query += $"completedstatus={completedStatus}";
+        }
+
+        if (!string.IsNullOrEmpty(titleContains))
+        {
+            query += string.IsNullOrEmpty(query) ? "?" : "&";
+            query += $"titlecontains={titleContains}";
+        }
+
+        return query;
+    }
 }
